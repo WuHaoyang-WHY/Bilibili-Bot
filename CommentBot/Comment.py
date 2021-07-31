c630d2fa5186d14364299d3424516d8575db1136
haoyangwu <wuhaoyang2@tal.com>
2021-07-31 22:55:05 +0800

    add dynamic comment

--- CommentBot/Comment.py
+++ CommentBot/Comment.py
@@ -1,6 +1,22 @@
 #!/usr/bin/env python 
 # -*- coding: utf-8 -*-
-# @Time    : 2021/7/29 9:20 下午
-# @Author  : Haoyang WU
-# @Email   : haoyang2@andrew.cmu.edu
-# @Description:
+import asyncio
+from bilibili_api import video, Credential, comment, bvid2aid
+
+
+# comment the video. must need either av_id or bv_id.
+async def comment_video(text, credential, av_id=None, bv_id=None):
+    if av_id is None:
+        if bv_id is None:
+            raise Exception("请至少提供 bvid 和 aid 中的其中一个参数。")
+        # BV_ID TO AV_ID
+        av_id = bvid2aid(bv_id)
+
+        # 给视频评论
+    await comment.send_comment(text=text, oid=av_id, type_=comment.ResourceType.VIDEO, credential=credential)
+
+
+# 评论动态
+async def comment_dynamic(text, credential, oid):
+    await comment.send_comment(text=text, oid=oid, type_=comment.ResourceType.DYNAMIC, credential=credential)
+
